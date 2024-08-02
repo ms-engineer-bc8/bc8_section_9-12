54aadee4a04de55b2e756b5106bd68ee41f0b89f
Kumi-H <935garden@gmail.com>
2024-08-02 18:52:44 +0900

    バックエンド: activityスキーマ更新

--- apps/backend/app/routers/activities.py
+++ apps/backend/app/routers/activities.py
@@ -10,10 +10,30 @@ router = APIRouter()
     tags=["activities"],
     response_model=List[ActivityCategoryResponse],
 )
-def get_activities(category_id):
-    return {"message": "Activites got"}
+def get_activities(category_id: int):
+    return [
+        ActivityCategoryResponse(
+            subcategory="リムジン",
+            image="",
+            provider_name="OneRide Limousine",
+            time_zone="夜",
+            solo_level="上級",
+            likes_count=10,
+            favorites_count=20,
+        )
+    ]
 
 
 @router.get("/{activity_id}", tags=["activities"], response_model=ActivityResponse)
 def get_activity(activity_id: int):
-    return {"message": "Activity got"}
+    return ActivityResponse(
+        provider_name="OneRide Limousine",
+        image_large="",
+        title="ソロリムジンといえばここ！特別な日のご褒美に",
+        discription="自由が丘駅から徒歩1分に位置する「OneRide Limousine」。豪華なレザーシートでくつろぎ、最新のエンタメを満喫しながら、まるで映画の主人公に。お一人さま専用のリムジンで、ちょっとしたセレブ気分を味わってください。特別な時間を「OneRide Limousine」でどうぞ！",
+        plan_name="【新宿ALTA発】東京の夜景を独り占め！ソロリムジンプラン（ドレスコード付き）",
+        price=25000,
+        image_small="",
+        coupon_discount_rate=10,
+        url="https://lalalimousine.com/",
+    )
