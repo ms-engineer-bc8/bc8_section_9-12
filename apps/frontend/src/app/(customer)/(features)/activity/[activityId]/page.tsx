5a76a64f873385f6793340a54a8f6e605a39ac8a
Mashiro1230 <risa.k.with@gmail.com>
2024-08-04 15:25:20 +0900

    フロントエンド：カテゴリページ作成

--- apps/frontend/src/app/(customer)/(features)/activity/[activityId]/page.tsx
+++ apps/frontend/src/app/(customer)/(features)/activity/[activityId]/page.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Heading from "@/app/components/ui-elements/heading";
+import Link from "next/link";
 
-const SpecialList = () => {
+const Limousine = () => {
     return (
-        <Heading>
-            スペシャル体験ソロ活
-        </Heading>
-    )
-}
+        <div className="container mx-auto p-5">
+            <p className="text-center">プライベートな空間で特別なひとときを</p>
+            <Heading>SkyDrive（ルーティング設定する）</Heading>
+            <p className="text-center">自由が丘駅から徒歩1分に位置する「SkyDrive」。豪華なレザーシートでくつろぎ、最新のエンタメを満喫しながら、まるで映画の主人公に。お一人さま専用のリムジンで、ちょっとしたセレブ気分を味わってください。特別な時間を「OneRide Limousine」でどうぞ！</p>
+            <h3>PLAN</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div><p>写真</p></div>
+            <div><p>【新宿ALTA発】東京の夜景を独り占め！ソロリムジンプラン（ドレスコード付き）</p>
+            <h4>サービス</h4>
+            <li>ウェルカムドリンク</li>
+            <li>写真撮影</li>
+            <li>ドレスコード</li>
+            <h4>25,000円</h4>
+            <button>予約する</button>
+            </div>
+            </div>
+        </div>
+    );
+};
 
-export default SpecialList;
+export default Limousine;
--- apps/frontend/src/app/components/ui-parts/list.tsx
+++ apps/frontend/src/app/components/ui-parts/list.tsx
@@ -10,22 +10,22 @@ const CategoryList = () => {
         <div className="flex justify-center items-center mt-1">
             <ul>
                 <li className="p-2">
-                    <Link href="/activity/1">
+                    <Link href="/category/1">
                         <ActiveLink>アクティブソロ活</ActiveLink>
                     </Link>
                 </li>
                 <li className="p-2">
-                    <Link href="/activity/2">
+                    <Link href="/category/2">
                         <SpecialLink>スペシャル体験ソロ活</SpecialLink>
                     </Link>
                 </li>
                 <li className="p-2">
-                    <Link href="/activity/3">
+                    <Link href="/category/3">
                         <RelaxLink>リラックス系ソロ活</RelaxLink>
                     </Link>
                 </li>
                 <li className="p-2">
-                    <Link href="/activity/4">
+                    <Link href="/category/4">
                         <GourmetLink>グルメ堪能ソロ活</GourmetLink>
                     </Link>
                 </li>
