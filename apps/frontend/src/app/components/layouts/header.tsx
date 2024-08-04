5596a22d99874c4005cbec4a1bcbd4873cb105f0
Mashiro1230 <risa.k.with@gmail.com>
2024-08-04 16:31:50 +0900

    フロントエンド：ヘッダーのナビゲーション修正&ロゴ欄追加

--- apps/frontend/src/app/components/layouts/header.tsx
+++ apps/frontend/src/app/components/layouts/header.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 import Link from "next/link";
+import { NAVIGATION_LINKS } from "@/app/commons/constants/constants";
 
-const navLinks = [
-    { label: "ソロ活を探す", href: "/" },
-    { label: "ソロ活をシェアする", href: "/share" },
-    { label: "マイページ", href: "/user" },
-];
-
-export default function Header() {
+export const NavigationMenu = () => {
     return (
-        <header>
-            <nav>
-                <ul className="flex justify-center max-w-full my-5">
-                    {navLinks.map((link) => (
-                        <li key={link.href} className="p-5">
-                            <Link href={link.href}>
-                                <p className="no-underline hover:underline hover:underline-offset-8 transition-all font-semibold">
-                                    {link.label}
-                                </p>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+        <nav>
+            <ul className="flex justify-end">
+                {NAVIGATION_LINKS.map((link) => (
+                    <li key={link.href} className="p-5">
+                        <Link href={link.href}>
+                            <p className="no-underline hover:underline hover:underline-offset-8 transition-all font-semibold">
+                                {link.label}
+                            </p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export const Header = () => {
+    return (
+        <header className="flex items-center justify-between px-6 py-4">
+            <h3 className="text-xl font-semibold">LOGO</h3>
+            <NavigationMenu />
         </header>
     );
-}
+};
+
+export default Header;
