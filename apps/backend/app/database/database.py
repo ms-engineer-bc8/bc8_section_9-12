b6db6c6a82cb80e5667ce6d424b0b8d8933571c2
Kumi-H <935garden@gmail.com>
2024-08-12 14:10:51 +0900

    バックエンド: POST,PUT /reviews 作成

--- apps/backend/app/database/database.py
+++ apps/backend/app/database/database.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-# from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
 load_dotenv()
