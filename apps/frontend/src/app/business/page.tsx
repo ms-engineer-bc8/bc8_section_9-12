64a33ce3a4c9b989aa2d04994df09274b24fcc6a
Mashiro1230 <risa.k.with@gmail.com>
2024-08-14 00:34:10 +0900

    フロントエンド：ホットリロード設定

--- apps/frontend/src/app/business/page.tsx
+++ apps/frontend/src/app/business/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function BusinessLp() {
     return (
         <div>
-            <h2 className="flex justify-center text-2xl font-semibold">このページがLPですかね...</h2>
+            <h2 className="flex justify-center text-2xl font-semibold">このページがLPですかね！</h2>
         </div>
     );
 }
