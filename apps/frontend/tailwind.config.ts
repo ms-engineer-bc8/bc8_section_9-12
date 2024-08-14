3382b174e60580318025557b2a96549f64e12ad9
Mashiro1230 <risa.k.with@gmail.com>
2024-08-14 17:58:09 +0900

    フロントエンド：グラデーション設定

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -20,8 +20,6 @@ const config: Config = {
                     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
                 "custom-gradient":
                     "linear-gradient(90deg, #FFDEE9 0%, #B5FFFC 100%)",
-                "noise": "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.25'/%3E%3C/svg%3E\")",
-                "custom-gradient-with-noise": "linear-gradient(90deg, #FFDEE9 0%, #B5FFFC 100%), url(\"data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.25'/%3E%3C/svg%3E\")",
             },
             colors: {
                 black: { DEFAULT: "#333333" },
