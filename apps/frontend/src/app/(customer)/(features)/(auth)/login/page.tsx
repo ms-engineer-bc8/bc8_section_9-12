1cd31d531e30fca4cef820aa97c7eb016dafa428
Kumi-H <935garden@gmail.com>
2024-08-16 11:51:49 +0900

    フロントエンド: resolverとreact-toastifyインストール

--- apps/frontend/src/app/(customer)/(features)/(auth)/login/page.tsx
+++ apps/frontend/src/app/(customer)/(features)/(auth)/login/page.tsx
@@ -2,22 +2,26 @@
 
 import React from "react";
 import Link from "next/link";
-import { useState } from "react";
+import { toast } from "react-toastify";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AuthSchema, authSchema } from "@/app/commons/types/types";
 import { useAuth } from "@/app/commons/auth/firebaseConfig";
 
 export default function CustomerLogIn() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
   const { signIn } = useAuth();
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setEmail(e.target.value);
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setPassword(e.target.value);
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<AuthSchema>({
+    mode: "onBlur",
+    resolver: zodResolver(authSchema),
+  });
 
-  const handleSignIn = async (event: React.FormEvent): Promise<void> => {
-    event.preventDefault();
-
+  const onSubmit = async (data: FieldValues) => {
+    const { email, password } = data;
     try {
       await signIn(email, password);
       console.log("Sign-in successful");
@@ -29,16 +33,15 @@ export default function CustomerLogIn() {
   return (
     <div className="container mx-auto max-w-md px-4 py-8">
       <h2 className="text-2xl font-bold text-center mb-6">ログイン</h2>
-      <form className="space-y-6" onSubmit={handleSignIn}>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         <div>
           <label className="block text-sm font-medium text-gray-700 mb-1">
             メールアドレス
           </label>
           <input
+            {...register("email")}
             id="email"
             type="email"
-            value={email}
-            onChange={handleEmailChange}
             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             placeholder="abc@test.com"
           />
@@ -48,10 +51,9 @@ export default function CustomerLogIn() {
             パスワード
           </label>
           <input
+            {...register("password")}
             id="password"
             type="password"
-            value={password}
-            onChange={handlePasswordChange}
             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             placeholder="●●●●●●●●"
           />
@@ -60,6 +62,7 @@ export default function CustomerLogIn() {
           <button
             type="submit"
             className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            disabled={isSubmitting}
           >
             ログインする
           </button>
