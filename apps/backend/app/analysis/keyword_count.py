c2f63ffcca840b1bb480bb7dbb5b4f846187d076
Kumi-H <935garden@gmail.com>
2024-08-18 14:00:38 +0900

    バックエンド: japanize-matplotlibインストール

--- apps/backend/app/analysis/keyword_count.py
+++ apps/backend/app/analysis/keyword_count.py
@@ -1,2 +1,22 @@
 import matplotlib.pyplot as plt
+import japanize_matplotlib  # noqa: F401
+import matplotlib.dates as dates
 import pandas as pd
+from datetime import datetime, timedelta
+
+# サンプルデータの作成
+data = {
+    "x": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    "y": [10, 20, 25, 30, 35, 50, 40, 45, 50, 20, 35, 20],
+}
+
+data["x"] = [datetime(2024, month, 1) for month in data["x"]]
+
+df = pd.DataFrame(data)
+
+# グラフの作成
+fig, ax = plt.subplots()
+ax.plot(df["x"], df["y"], marker="o")
+ax.set(title="誕生日")
+
+plt.savefig("result_weekly_figure.png")
