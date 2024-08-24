c49d5cdaf0f308c0bdcf27bf0c9da7a54288b448
Kumi-H <935garden@gmail.com>
2024-08-24 21:00:11 +0900

    フロントエンド: グローバル変数作成

--- apps/frontend/src/app/business/lp/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function BusinessLp() {
-    return (
-        <div>
-            <h2 className="flex justify-center text-2xl font-semibold">このページがLP</h2>
-        </div>
-    );
-}
--- apps/frontend/src/app/business/page.tsx
+++ apps/frontend/src/app/business/page.tsx
@@ -1,32 +1,23 @@
 "use client";
 
 import React from "react";
+import { useToken } from "../commons/contexts/contexts";
 
-export default function Report() {
+export default function BizHome() {
+  const { token } = useToken();
+  console.log("Bizトークン", token)
+
   return (
-    <div className="container flex justify-center mx-auto p-3">
-      <div className="w-96">
-        <form className="max-w-md mx-auto flex">
-          <input
-            type="search"
-            id="search"
-            className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="（例）リムジン"
-          />
-          <button
-            type="submit"
-            className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            検索
-          </button>
-        </form>
+    <div className="flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold my-4">Business Top</h1>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          新規登録（有料）
+        </button>
       </div>
-      <button
-        type="submit"
-        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        検索
-      </button>
     </div>
   );
 }
