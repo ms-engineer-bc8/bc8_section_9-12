3911a88966dfe7a4e485e2c1aeb1287edac04d8c
Mashiro1230 <risa.k.with@gmail.com>
2024-09-08 22:56:00 +0900

    フロントエンド：LP修正

--- apps/frontend/src/app/components/layouts/header.tsx
+++ apps/frontend/src/app/components/layouts/header.tsx
@@ -13,67 +13,98 @@ import Image from "next/image";
 import { useToken } from "@/app/commons/contexts/contexts";
 
 const CustomerNavigationMenu = () => (
-  <nav className="h-full flex items-center">
-    <ul className="flex justify-center h-full">
-      {CUSTOMER_NAVIGATION_LINKS.map(({ href, label }) => (
-        <li key={href} className="flex items-center px-5">
-          <Link href={href}>
-            <p className="no-underline font-semibold hover:text-pink-500 transition-all text-lg">
-              {label}
-            </p>
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
+    <nav className="h-full flex items-center">
+        <ul className="flex justify-center h-full">
+            {CUSTOMER_NAVIGATION_LINKS.map(({ href, label }) => (
+                <li key={href} className="flex items-center px-5">
+                    <Link href={href}>
+                        <p className="no-underline font-semibold hover:text-pink-500 transition-all text-lg">
+                            {label}
+                        </p>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </nav>
 );
 
 export const CustomerHeader = () => {
-  const { token } = useToken();
-  return (
-    <header className="relative">
-      <div className="mx-auto">
-        <div className="flex items-center justify-between px-10 py-2 h-24">
-          <div className="flex-shrink-0">
-            <Link href="/">
-              <div className="text-xl font-bold">
-                <Image src={Logo} width={150} height={50} alt="Logo" />
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center h-full text-lg">
-            <CustomerNavigationMenu />
-            <div className="ml-5">
-              {token ? (
-                <UserMenu iconSrc={CustomerLoginIconImage} />
-              ) : (
-                <UserMenu iconSrc={CustomerLogoutIconImage} />
-              )}
+    const { token } = useToken();
+    return (
+        <header className="relative">
+            <div className="mx-auto">
+                <div className="flex items-center justify-between px-10 py-2 h-24">
+                    <div className="flex-shrink-0">
+                        <Link href="/">
+                            <div className="text-xl font-bold">
+                                <Image
+                                    src={Logo}
+                                    width={150}
+                                    height={50}
+                                    alt="Logo"
+                                />
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="flex items-center h-full text-lg">
+                        <CustomerNavigationMenu />
+                        <div className="ml-5">
+                            {token ? (
+                                <UserMenu iconSrc={CustomerLoginIconImage} />
+                            ) : (
+                                <UserMenu iconSrc={CustomerLogoutIconImage} />
+                            )}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
+        </header>
+    );
 };
 
 export const BusinessHeader = () => (
-  <header className="relative">
-    <div className="mx-auto">
-      <div className="flex items-center justify-between px-10 py-2 h-24">
-        <div className="flex-shrink-0">
-          <Link href="/business">
-            <div className="text-xl font-bold">
-              <Image src={BizLogo} width={260} height={50} alt="Logo" />
+    <header className="relative">
+        <div className="mx-auto">
+            <div className="flex items-center justify-between px-10 py-2 h-24">
+                <div className="flex-shrink-0">
+                    <Link href="/business">
+                        <div className="text-xl font-bold">
+                            <Image
+                                src={BizLogo}
+                                width={260}
+                                height={50}
+                                alt="Logo"
+                            />
+                        </div>
+                    </Link>
+                </div>
+                <div className="flex items-center h-full text-lg">
+                    <div className="ml-5">
+                        <UserMenu iconSrc={BizIconImage} />
+                    </div>
+                </div>
             </div>
-          </Link>
         </div>
-        <div className="flex items-center h-full text-lg">
-          <div className="ml-5">
-            <UserMenu iconSrc={BizIconImage} />
-          </div>
+    </header>
+);
+
+export const LandingPageHeader = () => (
+    <header className="relative">
+        <div className="mx-auto">
+            <div className="flex items-center justify-between px-10 py-2 h-24">
+                <div className="flex-shrink-0">
+                    <Link href="/">
+                        <div className="text-xl font-bold">
+                            <Image
+                                src={Logo}
+                                width={150}
+                                height={50}
+                                alt="Logo"
+                            />
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </header>
+    </header>
 );
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -6,33 +6,33 @@ import ToastProvider from "./components/layouts/toastProvider";
 import { TokenProvider } from "./commons/contexts/contexts";
 
 const zenKakuGothicNew = Zen_Kaku_Gothic_New({
-  weight: ["400", "500", "700", "900"],
-  subsets: ["latin"],
+    weight: ["400", "500", "700", "900"],
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Soloco",
-  description:
-    "ソロの女性が新しいソロ活に出会い、お互いのソロ活をシェアするためのアプリです。",
+    title: "Soloco",
+    description:
+        "ソロの女性が新しいソロ活に出会い、お互いのソロ活をシェアするためのアプリです。",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body className={zenKakuGothicNew.className}>
-        <div className="min-h-screen flex flex-col">
-          <main className="flex-grow">
-            <Background />
-            <ToastProvider>
-              <TokenProvider>{children}</TokenProvider>
-            </ToastProvider>
-          </main>
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="ja">
+            <body className={zenKakuGothicNew.className}>
+                <div className="min-h-screen flex flex-col">
+                    <main className="flex-grow">
+                        <Background />
+                        <ToastProvider>
+                            <TokenProvider>{children}</TokenProvider>
+                        </ToastProvider>
+                    </main>
+                </div>
+            </body>
+        </html>
+    );
 }
