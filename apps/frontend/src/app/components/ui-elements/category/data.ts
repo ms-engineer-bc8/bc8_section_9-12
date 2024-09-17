cc8796a235791d632b0782a265d281f325567703
Mashiro1230 <risa.k.with@gmail.com>
2024-09-18 00:11:48 +0900

    フロントエンド：TOPページの画像修正

--- apps/frontend/src/app/components/ui-elements/category/data.ts
+++ apps/frontend/src/app/components/ui-elements/category/data.ts
@@ -1,7 +1,10 @@
-import ActiveCategoryImage from "../../../../../public/images/category/active.jpg";
-import SpecialCategoryImage from "../../../../../public/images/category/special.jpg";
-import RelaxCategoryImage from "../../../../../public/images/category/relax.jpg";
-import GourmetCategoryImage from "../../../../../public/images/category/gourmet.jpg";
+import { getImageUrl } from "@/app/commons/utils/imageUtils";
+
+// S3から画像取得
+const ActiveCategoryImage = getImageUrl("active.jpg");
+const SpecialCategoryImage = getImageUrl("special.jpg");
+const RelaxCategoryImage = getImageUrl("relax.jpg");
+const GourmetCategoryImage = getImageUrl("gourmet.jpg");
 
 export const categoryData = [
     {
