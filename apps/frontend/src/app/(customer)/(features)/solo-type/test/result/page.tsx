bf58d18806a564bd259b70ad0f6d88f2b3fc6539
Mashiro1230 <risa.k.with@gmail.com>
2024-10-01 23:57:22 +0900

    フロントエンド：診断結果をSSR

--- apps/frontend/src/app/(customer)/(features)/solo-type/test/result/page.tsx
+++ apps/frontend/src/app/(customer)/(features)/solo-type/test/result/page.tsx
@@ -1,56 +1,29 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Image from "next/image";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useToken } from "@/app/commons/contexts/contexts";
-import { toast } from "react-toastify";
+import { redirect } from "next/navigation";
 import { getImageUrl } from "@/app/commons/utils/imageUtils";
 import { PinkButton } from "@/app/components/ui-elements/button/button";
 import { SoloTypeResultProp } from "@/app/commons/types/types";
 
 const SpecialType = getImageUrl("special.png");
 
-export default function SoloTypeResult() {
-    const { token } = useToken();
-    const router = useRouter();
-
-    if (token === "") {
-        router.push("/login");
+async function getSoloType(solo_type: string | null, userId: string | null): Promise<SoloTypeResultProp | null> {
+    if (solo_type && userId) {
+        return { solo_type, userId };
     }
+    return null;
+}
 
-    const [soloTypeData, setSoloTypeData] = useState<SoloTypeResultProp | null>(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const searchParams = useSearchParams();
+export default async function SoloTypeResult({
+    searchParams,
+}: {
+    searchParams: { solo_type: string; userId: string };
+}) {
+    const soloTypeData = await getSoloType(searchParams.solo_type, searchParams.userId);
 
-    useEffect(() => {
-        const fetchSoloType = () => {
-            try {
-                const solo_type = searchParams.get("solo_type");
-                const userId = searchParams.get("userId");
-                if (solo_type && userId) {
-                    setSoloTypeData({ solo_type, userId });
-                } else {
-                    throw new Error(
-                        "ソロ活タイプまたはユーザーIDが見つかりません"
-                    );
-                }
-            } catch (error) {
-                console.error("Error:", error);
-                toast.error(
-                    "ソロ活タイプの取得に失敗しました。もう一度診断を行ってください。"
-                );
-                router.push("/solo-type/test");
-            } finally {
-                setIsLoading(false);
-            }
-        };
-        fetchSoloType();
-    }, [router, searchParams]);
-
-    const handleReturnToTop = () => {
-        router.push("/");
-    };
+    if (!soloTypeData) {
+        redirect("/solo-type/test");
+    }
 
     return (
         <div className="container mx-auto max-w-xl p-4">
@@ -58,43 +31,33 @@ export default function SoloTypeResult() {
                 <h2 className="text-2xl font-bold text-center mb-6">
                     あなたのソロ活タイプは
                 </h2>
-                {soloTypeData && soloTypeData.solo_type ? (
-                    <>
-                        <h2 className="text-center mb-6 text-pink-500 font-bold">
-                            「{soloTypeData.solo_type}」
-                        </h2>
-                        <div className="flex justify-center mb-6">
-                            <Image
-                                src={SpecialType}
-                                alt={soloTypeData.solo_type}
-                                width={350}
-                                height={350}
-                            />
-                        </div>
-                        <div>
-                            <p className="mb-10 mt-4 px-14 text-base text-center">
-                                現代の優雅な冒険者、それがあなた！
-                                <br />
-                                財布の中身は「一生に一度の体験」のため💃
-                                <br />
-                                街でリムジンを見れば「私の車かしら」とつぶやき、休日の予定は「気球で空中ピクニック」🌌
-                                <br />
-                                周りはきっと羨望の眼差し👀
-                            </p>
-                        </div>
-                    </>
-                ) : (
-                    <p className="text-center mb-6">
-                        ソロ活タイプが見つかりませんでした
+                <h2 className="text-center mb-6 text-pink-500 font-bold">
+                    「{soloTypeData.solo_type}」
+                </h2>
+                <div className="flex justify-center mb-6">
+                    <Image
+                        src={SpecialType}
+                        alt={soloTypeData.solo_type}
+                        width={350}
+                        height={350}
+                    />
+                </div>
+                <div>
+                    <p className="mb-10 mt-4 px-14 text-base text-center">
+                        現代の優雅な冒険者、それがあなた！
+                        <br />
+                        財布の中身は「一生に一度の体験」のため💃
+                        <br />
+                        街でリムジンを見れば「私の車かしら」とつぶやき、休日の予定は「気球で空中ピクニック」🌌
+                        <br />
+                        周りはきっと羨望の眼差し👀
                     </p>
-                )}
+                </div>
                 <div className="flex justify-center px-14">
-                    <PinkButton onClick={handleReturnToTop}>
-                        TOPに戻る
-                    </PinkButton>
+                    <PinkButton href="/">TOPに戻る</PinkButton>
                 </div>
             </div>
         </div>
     );
-};
+}
 
