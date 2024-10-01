d8c70406185da7eb3028dc7ef5ad2aae641f86a2
Kumi-H <935garden@gmail.com>
2024-10-01 23:05:08 +0900

    バックエンド: Firebase初期化作成

--- apps/backend/app/core/auth.py
+++ apps/backend/app/core/auth.py
@@ -1,10 +1,12 @@
-import os
 import firebase_admin
 from firebase_admin import credentials
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
+credentials_path = os.path.join("app/static/credentials", "bc8-final-project-firebase-adminsdk-w7c4f-9b2bbef84a.json")
 
-
-cred = credentials.Certificate(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-default_app = firebase_admin.initialize_app(cred)
+def admin():
+    print("パス", credentials_path)
+    cred = credentials.Certificate(credentials_path)
+    default_app = firebase_admin.initialize_app(cred)
+    
+    return default_app
