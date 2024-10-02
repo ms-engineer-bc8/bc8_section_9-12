f7303b94e74e1133329397bc562d1b248a83a1c2
Kumi-H <935garden@gmail.com>
2024-10-02 22:16:41 +0900

    バックエンド: ファイル名を環境変数に設定

--- apps/backend/app/core/auth.py
+++ apps/backend/app/core/auth.py
@@ -1,8 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials
 import os
+from dotenv import load_dotenv
 
-credentials_path = os.path.join("app/static/credentials", "bc8-final-project-firebase-adminsdk-w7c4f-9b2bbef84a.json")
+load_dotenv()
+
+key_json = os.getenv("FIREBASE_CREDENTIALS")
+credentials_path = os.path.join("app/static/credentials", key_json)
 
 def admin():
     print("パス", credentials_path)
