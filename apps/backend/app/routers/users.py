e1337af519b553d4de6ef6d5211f512d9837f391
Kumi-H <935garden@gmail.com>
2024-10-05 21:19:01 +0900

    バックエンド: try-except追加

--- apps/backend/app/routers/users.py
+++ apps/backend/app/routers/users.py
@@ -11,11 +11,13 @@ from app.schemas.user import (
     UserTypeResponse,
 )
 from app.analysis.solo_type import get_solo_type_id
+from app.core.logging_config import setup_logging
 
 router = APIRouter()
+logger = setup_logging(__name__)
 
 
-# TODO: 認証成功していたら渡す emailの取得方法を検討
+# TODO: MYページ機能を追加時に作りこむ
 @router.get(
     "/me", tags=["users"], response_model=UserResponse, status_code=status.HTTP_200_OK
 )
@@ -45,11 +47,30 @@ def get_user_me(db: Session = Depends(get_db)):
     status_code=status.HTTP_201_CREATED,
 )
 def post_user(item: UserCreate, db: Session = Depends(get_db)):
-    user_item = User(**item.model_dump())
-    db.add(user_item)
-    db.commit()
-    db.refresh(user_item)
+    try:
+        user_item = User(**item.model_dump())
+        nickname = db.query(User).filter(User.nickname == user_item.nickname).first()
 
+        if nickname:
+            raise HTTPException(status_code=409, detail="Conflict occurred")
+
+        db.add(user_item)
+        db.commit()
+        db.refresh(user_item)
+
+    except HTTPException as http_exception:
+        logger.warning("Nickname already exists")
+        raise http_exception
+
+    except Exception as e:
+        logger.exception("Raise Exception: %s", str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        )
+
+    logger.info("User created")
+    logger.debug("id", item.id)
     return UserIdResponse(id=user_item.id)
 
 
@@ -60,31 +81,46 @@ def post_user(item: UserCreate, db: Session = Depends(get_db)):
     status_code=status.HTTP_200_OK,
 )
 def put_user_type(user_id: int, item: UserType, db: Session = Depends(get_db)):
-    user_item = db.query(User).filter(User.id == user_id).first()
+    try:
+        user_item = db.query(User).filter(User.id == user_id).first()
 
-    if user_item is None:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+        if user_item is None:
+            raise ValueError("User not found")
 
-    solo_type_id = get_solo_type_id(item)
-    solo_type_name = (
-        db.query(SoloType.name).filter(SoloType.id == solo_type_id).scalar()
-    )
+        solo_type_id = get_solo_type_id(item)
+        solo_type_name = (
+            db.query(SoloType.name).filter(SoloType.id == solo_type_id).scalar()
+        )
 
-    user_item.solo_level = item.solo_level
-    user_item.activity_preference = item.activity_preference
-    user_item.time_preference = item.time_preference
-    user_item.is_planned = item.is_planned
-    user_item.weekend_plan_preference = item.weekend_plan_preference
-    user_item.after_work_preference = item.after_work_preference
-    user_item.comfort_adventure = item.comfort_adventure
-    user_item.solo_type_id = solo_type_id
+        user_item.solo_level = item.solo_level
+        user_item.activity_preference = item.activity_preference
+        user_item.time_preference = item.time_preference
+        user_item.is_planned = item.is_planned
+        user_item.weekend_plan_preference = item.weekend_plan_preference
+        user_item.after_work_preference = item.after_work_preference
+        user_item.comfort_adventure = item.comfort_adventure
+        user_item.solo_type_id = solo_type_id
 
-    db.commit()
-    db.refresh(user_item)
+        db.commit()
+        db.refresh(user_item)
 
+    except ValueError as ve:
+        logger.warning(str(ve))
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail=str(ve))
+
+    except Exception as e:
+        logger.exception("Raise Exception: %s", str(e))
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
+        )
+
+    logger.info("User updated")
+    logger.debug("id", item.id)
     return UserTypeResponse(solo_type=solo_type_name)
 
 
+# TODO: MYページ機能を追加時に作りこむ
 @router.put("/{user_id}", tags=["users"], status_code=status.HTTP_200_OK)
 def put_user(user_id: int, item: UserUpdate, db: Session = Depends(get_db)):
     user_item = db.query(User).filter(User.id == user_id).first()
