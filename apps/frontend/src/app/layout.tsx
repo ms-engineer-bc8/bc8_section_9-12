97551fd12700b8664ab7052334ed38f1d5700ddf
Mashiro1230 <risa.k.with@gmail.com>
2024-10-15 00:30:21 +0900

    フロントエンド：token修正

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Zen_Kaku_Gothic_New } from "next/font/google";
 import Background from "./components/layouts/background";
 import "./commons/styles/globals.css";
 import ToastProvider from "./components/layouts/toastProvider";
-import { TokenProvider } from "./commons/contexts/contexts";
 
 const zenKakuGothicNew = Zen_Kaku_Gothic_New({
     weight: ["400", "500", "700", "900"],
@@ -27,9 +26,7 @@ export default function RootLayout({
                 <div className="min-h-screen flex flex-col">
                     <main className="flex-grow">
                         <Background />
-                        <ToastProvider>
-                            <TokenProvider>{children}</TokenProvider>
-                        </ToastProvider>
+                        <ToastProvider>{children}</ToastProvider>
                     </main>
                 </div>
             </body>
